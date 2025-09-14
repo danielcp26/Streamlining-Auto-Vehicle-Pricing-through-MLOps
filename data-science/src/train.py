469a71bb5582be9d335ea3e0296f698a114eef9f
danielcp26 <danielcp.26@hotmail.com>
2025-09-14 17:48:35 -0600

    recommendations

--- data-science/src/train.py
+++ data-science/src/train.py
@@ -83,3 +83,13 @@ if __name__ == "__main__":
 
     mlflow.end_run()
 
+
+#github repo link: https://github.com/danielcp26/Streamlining-Auto-Vehicle-Pricing-through-MLOps
+
+'''Recomendations:
+
+- Implement a more robust data cleaning and quality assurance process so the pipeline will always treat outliers, missing values or any other inconsistency when new data comes into the project.
+
+- Try other regression models that might perform better like xgboost regressor or Neural Networks if we want to improve the accuracy of the model.
+
+'''
